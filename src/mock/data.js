355a257ebf5359b7ca4769edf0c8aa4537eac650
stefan01-dev <stefan01.dev@gmail.com>
2021-01-11 06:14:50 +0200

    updating portofolio

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,32 +9,26 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Greetings, my names is ',
+  name: 'Stefan Dudu',
+  subtitle: `I'm de developer you don't know yet you need`,
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'profile.jpeg',
+  paragraphOne:
+    'Stefan Dudu is very passionate about the web development industry and information technology. He has a `long run` mindset, eager to stay in tune with the latest and modern technologies.',
+  paragraphTwo:
+    'His hunger for knowledge and his perseveration are the reasons of why he can achieve many things in a short period of time and learn a lot. He considers himself a well-organised person, problem solver and he loves to work in a team on ambitious projects with the right people. He enjoys working on the front-end with React but he is very flexible with a lot of technologies.',
+  paragraphThree:
+    'Besides programming, his hobbies are weight-lifting, reading, outdoor walks, trekking, reading and sleeping.',
+  resume: 'https://www.linkedin.com/in/stefan-dudu/', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
   {
     id: nanoid(),
     img: 'project.jpg',
