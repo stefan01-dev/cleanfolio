8468c6943932fc6f6fe08212a66852503f813945
stefan01-dev <stefan01.dev@gmail.com>
2021-01-13 05:15:32 +0200

    added particles, fixed positioning

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
+import Particles from 'react-particles-js';
 import { Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-scroll';
@@ -22,27 +23,86 @@ const Header = () => {
   }, []);
 
   return (
-    <section id="hero" className="jumbotron">
-      <Container>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
-          <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
-            <br />
-            {subtitle || "I'm the Unknown Developer."}
-          </h1>
-        </Fade>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-          <p className="hero-cta">
-            <span className="cta-btn cta-btn--hero">
-              <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
-              </Link>
-            </span>
-          </p>
-        </Fade>
-      </Container>
-    </section>
+    <>
+      <Particles
+        className="particles"
+        params={{
+          particles: {
+            number: {
+              value: 110,
+              density: {
+                enable: false,
+              },
+            },
+            color: {
+              value: '#d4cab2',
+            },
+            size: {
+              value: 6,
+              random: true,
+              anim: {
+                speed: 4,
+                size_min: 0.3,
+              },
+            },
+            line_linked: {
+              enable: false,
+            },
+            move: {
+              random: true,
+              speed: 1,
+              direction: 'top',
+              out_mode: 'out',
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: 'bubble',
+              },
+              onclick: {
+                enable: true,
+                mode: 'repulse',
+              },
+            },
+            modes: {
+              bubble: {
+                distance: 250,
+                duration: 2,
+                size: 0,
+                opacity: 0,
+              },
+              repulse: {
+                distance: 400,
+                duration: 4,
+              },
+            },
+          },
+        }}
+      />
+      <section id="hero" className="jumbotron">
+        <Container>
+          <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+            <h1 className="hero-title">
+              {title || 'Hi, my name is'}{' '}
+              <span className="text-color-main">{name || 'Your Name'}</span>
+              <br />
+              {subtitle || "I'm the Unknown Developer."}
+            </h1>
+          </Fade>
+          <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+            <p className="hero-cta">
+              <span className="cta-btn cta-btn--hero">
+                <Link to="about" smooth duration={1000}>
+                  {cta || 'Know more'}
+                </Link>
+              </span>
+            </p>
+          </Fade>
+        </Container>
+      </section>
+    </>
   );
 };
 
